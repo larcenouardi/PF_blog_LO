720137eaa92ea4b3a1f793eed593baa21cf53784
larcen <91964970+larcenouardi@users.noreply.github.com>
2023-09-23 16:02:40 +0100

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user : 'postgres',
-    host : 'localhost',
-    database:'blog',
-    password:'0708007329',
-    port:'5432'
+  user : 'blog',
+  host : 'dpg-ck7fog08elhc73acpncg-a.oregon-postgres.render.com',
+  database:'blog_ukdm',
+  password:'CDmW1hCTIBO0bvyy2mmJNWUDxUehG8UA',
+  port:'5432'
 });
 
 module.exports = {
